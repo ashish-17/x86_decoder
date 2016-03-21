690a901e50f58d822481a3d83039f09a41b69e1f
ashish-17 <ashishj1707@gmail.com>
2016-03-21 14:58:45 -0400

    Fix the implementation of SIB addressing scaling factor

--- decoder.v
+++ decoder.v
@@ -1,6 +1,6 @@
 `include "register_file.v"
 
-`define SIZE_DECODE_REG 48
+`define SIZE_DECODE_REG 64
 
 module decoder(
     input i_clk,
@@ -17,12 +17,16 @@ module decoder(
     reg[`SIZE_DECODE_REG-1:0] decode_tmp_reg;
     reg[`SIZE_DECODE_REG-1:0] decode_hold_reg;
     reg[3:0] count_bytes_in_hold_reg;
+    reg[3:0] count_bytes_in_decode_reg;
+    reg[3:0] count_bytes_instr;
 
     initial begin
         decode_reg = `SIZE_DECODE_REG'b0;
         decode_tmp_reg = `SIZE_DECODE_REG'b0;
         decode_hold_reg = `SIZE_DECODE_REG'b0;
         count_bytes_in_hold_reg = 4'h0;
+        count_bytes_in_decode_reg = 4'h0;
+        count_bytes_instr = 4'h0;
     end
 
     always @(i_reset or i_data) begin
@@ -37,9 +41,11 @@ module decoder(
         if (!i_reset && i_ready) begin
             decode_tmp_reg = i_data;
             decode_reg = (decode_hold_reg | (decode_tmp_reg<<(8*count_bytes_in_hold_reg)));
-            count_bytes_in_hold_reg = count_bytes_in_hold_reg + 4; // Every time we get a 32 bit input from memory
+            count_bytes_in_decode_reg = count_bytes_in_hold_reg + 4; // Every time we get a 32 bit input from memory
+            
             if (decode_reg[7:0] != 8'h0f) begin // 1 Byte opcode
                 opcode = i_data[7:0];
+                count_bytes_instr = count_bytes_instr + 1; // Increase by 1 for one byte opcode
 
                 case(decode_reg[7:4])
                    4'h0: begin 
@@ -128,7 +134,7 @@ module decoder(
             2'b00: begin // Indirect addressing mode
                 if (data[10:8] == 3'b100) begin // SIB Addressing mode
                     if (data[23:22] > 0) // scaling present, TODO - verify 32 but displacement
-                        $sformat(result, "%s, (%s, %s*%d+%x)", get_reg(data[13:11]), get_reg(data[18:16]), get_reg(data[21:19]), data[23:22], data[55:24]);
+                        $sformat(result, "%s, (%s, %s*%d+%x)", get_reg(data[13:11]), get_reg(data[18:16]), get_reg(data[21:19]), get_scale(data[23:22]), data[55:24]);
                     else
                         $sformat(result, "%s, (%s, %s+%x)", get_reg(data[13:11]), get_reg(data[18:16]), get_reg(data[21:19]), data[55:24]);
                 end
@@ -143,7 +149,7 @@ module decoder(
             2'b01: begin // Same as above but with 8 bit displacement
                 if (data[10:8] == 3'b100) begin // SIB Addressing mode
                     if (data[23:22] > 0) // scaling present
-                        $sformat(result, "%s, (%s,%s*%d+%x)", get_reg(data[13:11]), get_reg(data[18:16]), get_reg(data[21:19]), data[23:22], data[31:24]);
+                        $sformat(result, "%s, (%s,%s*%d+%x)", get_reg(data[13:11]), get_reg(data[18:16]), get_reg(data[21:19]), get_scale(data[23:22]), data[31:24]);
                     else
                         $sformat(result, "%s, (%s, %s+%x)", get_reg(data[13:11]), get_reg(data[18:16]), get_reg(data[21:19]), data[31:24]);
                 end
@@ -165,6 +171,15 @@ module decoder(
         endcase
 
         return result;
+    endfunction
+
+    function [3:0] get_scale(input[1:0] scale_bits); // Scale value corresponding to scale bits
+        case(scale_bits)
+            2'b00: get_scale = 4'h1;
+            2'b01: get_scale = 4'h2;
+            2'b10: get_scale = 4'h4;
+            2'b11: get_scale = 4'h8;
+        endcase
     endfunction
 
     function [31:0] instruction_length(input[7:0] modrm); // Find instruction length other than opcode length and modrm byte (returns number of bytes)
