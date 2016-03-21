701d512c42cf69378a75721e20c992f62e8e264d
ashish-17 <ashishj1707@gmail.com>
2016-03-21 01:31:08 -0400

    Direct addressing part of mod/rm works

--- decoder.v
+++ decoder.v
@@ -64,6 +64,9 @@ module decoder(
                             mnemonic = "add";
                         
                         end
+                        else if (i_data[3:0] == 4'hB) begin
+                            mnemonic = "mov";
+                        end
                     end
                     
                     4'h9: begin 
@@ -92,10 +95,44 @@ module decoder(
                 // Not implemented
             end
 
-            $display("%x \t %x \t %s", i_data, opcode, mnemonic);
+            $display("%x \t %x \t %s - %s", i_data, opcode, mnemonic, decode_mod_reg_rm(i_data));
         end
         else begin
             instr_size <= 1'bx;
         end
     end
+
+    function string decode_mod_reg_rm(input[31:0] data);
+        string result = "";
+        case(data[15:14]) // Mod bits
+            2'b00: begin
+                
+            end
+
+            2'b01: begin
+            end
+
+            2'b10: begin
+            end
+
+            2'b11: begin // Direct addressing
+                result = {get_reg(data[13:11]), ", ", get_reg(data[10:8])};
+            end
+        endcase
+
+        return result;
+    endfunction
+
+    function string get_reg(input[2:0] code);
+        case (code)
+            3'b000: get_reg = "%eax";
+            3'b001: get_reg = "%ecx";
+            3'b010: get_reg = "%edx";
+            3'b011: get_reg = "%ebx";
+            3'b100: get_reg = "%esp";
+            3'b101: get_reg = "%ebp";
+            3'b110: get_reg = "%esi";
+            3'b111: get_reg = "%edi";
+        endcase
+    endfunction
 endmodule
